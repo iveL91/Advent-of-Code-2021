1d8fe6744f4006a9ad1099945f593d7a65235692
Dominik Schillo <dominik.schillo@posteo.de>
2021-12-01 08:28:22 +0100

    Fix tests for Day 01

--- src/test/day01/test.py
+++ src/test/day01/test.py
@@ -4,7 +4,7 @@ Tests for day 01.
 
 import unittest
 
-from src.main.day01.main import data_input, part_1
+from src.main.day01.main import data_input, part_1, part_2
 
 
 class TestDay01(unittest.TestCase):
@@ -14,25 +14,25 @@ class TestDay01(unittest.TestCase):
 
     def test_part_1(self) -> None:
         """
-        Testing part 1.
+        Testing part 1
         """
         subtest_list: list[tuple[str, int]] = [("test_data_1", 7),
                                                ("data", 1624)]
         for filename, expected_result in subtest_list:
             with self.subTest():
-                measurements: list[int] = data_input(filename)
+                measurements = data_input("../test/day01/" + filename)
                 self.assertEqual(expected_result, part_1(measurements))
 
     def test_part_2(self) -> None:
         """
-        Testing part 2.
+        Testing part 2
         """
         subtest_list: list[tuple[str, int]] = [("test_data_1", 5),
                                                ("data", 1653)]
         for filename, expected_result in subtest_list:
             with self.subTest():
-                measurements: list[int] = data_input(filename)
-                self.assertEqual(expected_result, part_1(measurements))
+                measurements = data_input("../test/day01/" + filename)
+                self.assertEqual(expected_result, part_2(measurements))
 
 
 if __name__ == '__main__':
